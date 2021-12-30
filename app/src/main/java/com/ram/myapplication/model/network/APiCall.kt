9a38a88f7910788277d288ed1087ff836d23b6fa
rkv <ramachandru6@gmail.com>
2021-12-30 20:57:03 +0530

    For retrofit class changed to object class

--- app/src/main/java/com/ram/myapplication/model/network/APiCall.kt
+++ app/src/main/java/com/ram/myapplication/model/network/APiCall.kt
@@ -3,12 +3,11 @@ package com.ram.myapplication.model.network
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class APiCall {
-    companion object {
-        fun getRetrofit() = Retrofit.Builder()
-            .baseUrl(ApiInterface.BASEURL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(ApiInterface::class.java)
-    }
+object APiCall {
+    fun getRetrofit() = Retrofit.Builder()
+        .baseUrl(ApiInterface.BASEURL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(ApiInterface::class.java)
+
 }
